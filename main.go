43789e404eea5c1e0ca5c31465e7efd798e2cb8a
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-11-29 11:02:14 -0800

    Crate app state and commands map and register the login handler function

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ehafenmaier/boot-dev-gator/internal/config"
 	"log"
+	"os"
 )
+
+type state struct {
+	config *config.Config
+}
 
 func main() {
 	// Read the configuration
@@ -13,18 +17,33 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Set the user name
-	err = cfg.SetUser("ehafenmaier")
+	// Create a new state
+	s := &state{
+		config: cfg,
+	}
+
+	// Create commands map
+	c := commands{
+		cmds: make(map[string]func(*state, command) error),
+	}
+
+	// Register the login command
+	c.register("login", handlerLogin)
+
+	// Check for the proper number of arguments
+	if len(os.Args) < 2 {
+		log.Fatal("usage: boot-dev-gator <command> [args...]")
+	}
+
+	// Split the command line arguments into a command and its arguments
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+
+	// Run the command
+	err = c.run(s, cmd)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Read the configuration again
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Print the configuration to the console
-	fmt.Printf("%+v\n", *cfg)
 }
