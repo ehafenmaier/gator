160dd69e57f90e0628e678737acc47d21c44736a
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-11-30 10:52:12 -0800

    Add user validation check to login handler function

--- login.go
+++ login.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("usage: login <username>")
 	}
 
-	err := s.config.SetUser(cmd.args[0])
+	if len(cmd.args[0]) == 0 {
+		return fmt.Errorf("username cannot be empty")
+	}
+
+	// Check if the user exists
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("user does not exist")
+	}
+
+	err = s.cfg.SetUser(cmd.args[0])
 	if err != nil {
 		return err
 	}
