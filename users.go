2de6a8c48753ac8b8704dd08c76dfb69c9e2df80
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-12-01 00:03:09 -0800

    Consolidate user handler functions into a single file

--- users.go
+++ users.go
@@ -8,6 +8,7 @@ import (
 	"time"
 )
 
+// Register user handler function
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("usage: register <username>")
@@ -43,6 +44,45 @@ func handlerRegister(s *state, cmd command) error {
 	}
 
 	fmt.Printf("Registered as %s\n", user.Name)
+
+	return nil
+}
+
+// Login user handler function
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("usage: login <username>")
+	}
+
+	if len(cmd.args[0]) == 0 {
+		return fmt.Errorf("username cannot be empty")
+	}
+
+	// Check if the user exists
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("user does not exist")
+	}
+
+	err = s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Logged in as %s\n", cmd.args[0])
+
+	return nil
+}
+
+// reset users handler function
+func handlerReset(s *state, _ command) error {
+	// Reset the users table in the database
+	err := s.db.ResetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("error resetting users in the database: %v", err)
+	}
+
+	fmt.Println("Database users reset")
 
 	return nil
 }
