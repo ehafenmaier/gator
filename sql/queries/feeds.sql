aba5dd5416151b8715dc97cacf42414949308efe
Erich Hafenmaier <ehafenmaier@gmail.com>
2024-12-03 17:57:41 -0800

    Create sqlc query to list all feeds in the database

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -2,3 +2,9 @@
 INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+-- name: GetAllFeeds :many
+SELECT f.id, f.created_at, f.updated_at, f.name, f.url, f.user_id,
+       u.name AS user_name
+FROM feeds f
+JOIN users u ON f.user_id = u.id;
